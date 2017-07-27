3310e484bfe3156804445376b83aa7864ace1fb8
kid <kid@kid-2.local>
2017-07-27 09:43:36 +0800

    Initial commit

---
+++ spider.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://cn.tripadvisor.com/Attractions-g60763-Activities-New_York_City_New_York.html'
+wedata = requests.get(url)
+soup = BeautifulSoup(wedata.text ,'lxml' )
+titles = soup.select('div.item.name > a')
+print(titles)
